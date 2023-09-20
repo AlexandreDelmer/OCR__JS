ff864da9ec6f599b42cdcd5deb367d59745a1601
Alexandre Delmer <delmer.alexandre1@gmail.com>
2023-09-20 21:49:20 +0200

    MàJ

--- script/script2.js
+++ script/script2.js
@@ -1,6 +1,21 @@
 //Créer une fonction pour choisir entre mots ou phrases
 
+function choixJeux(){
+    let choix = prompt("Entrez votre choix de jeux : 'mots' ou 'phrases'")
+    while(choix !== "mots" && choix !== "phrases"){
+        choix = prompt("Entrez votre choix de jeux : 'mots' ou 'phrases'")
+    }
+    return choix
+}
+
 //Créer une fonction pour jouer et saisir les mots ou phrases
+
+function jeux(listeProposition){
+    let score = 0
+    if(choix === "mots"){
+        
+    }
+}
 
 //Créer une fonction pour afficher le résultat
 
