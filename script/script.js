a89587d1be87291dd38df5b05f0ccf96ed2d6c10
Alexandre Delmer <delmer.alexandre1@gmail.com>
2023-09-20 13:58:08 +0200

    MàJ 20-09-2023

--- script/script.js
+++ script/script.js
@@ -1,7 +1,3 @@
-let score = 0
-let nbMotsProposes = listeMots.length
-let nbPhrasesProposes = listePhrases.length
-
 //On crée la fonction pour afficher le résultat du jeu
 function afficherResultat(score, nbMotsProposes){
     // On affiche le résultat
@@ -19,6 +15,7 @@ function choisirPhrasesOuMots(){
 
 //On crée la boucle de jeu
 function lancerBoucleDeJeu(listeProposition){
+    let score = 0
     for (let i = 0; i < listeProposition.length; i++ ){
         //On demande à l'utilisateur de saisir le mot indiqué
         let motUtilisateur = prompt("Entrez le mot : " + listeProposition[i])
@@ -31,4 +28,18 @@ function lancerBoucleDeJeu(listeProposition){
 }
 
 //C'est la fonction principale qui va s'occuper de lancer toutes les autres
-function lancerJeu()
+function lancerJeu(){
+    let choix = choisirPhrasesOuMots()
+    let score = 0
+    let nbMotsProposes = 0
+
+    if (choix === "mots"){
+        score = lancerBoucleDeJeu(listeMots)
+        nbMotsProposes = listeMots.length
+    } else {
+        score = lancerBoucleDeJeu(listePhrases)
+        nbMotsProposes = listePhrases.length
+    }
+    
+    afficherResultat(score, nbMotsProposes)
+}
---
+++ script/script2.js
@@ -0,0 +1,7 @@
+//Créer une fonction pour choisir entre mots ou phrases
+
+//Créer une fonction pour jouer et saisir les mots ou phrases
+
+//Créer une fonction pour afficher le résultat
+
+//Créer une fonction pour lancer le jeu
