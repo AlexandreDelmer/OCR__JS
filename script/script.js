75b8fe3b55a498243ca7b81b993644628ef0f7bb
Alexandre Delmer <delmer.alexandre1@gmail.com>
2023-09-25 14:04:35 +0200

    JS CreateElement

--- script/script.js
+++ script/script.js
@@ -1,2 +1,20 @@
-let listInputRadiot = document.querySelectorAll(".zoneChoix input")
-console.log(listInputRadiot)
+let userInput = document.getElementById("inputEcriture")
+console.log(userInput)
+
+let validationButton = document.getElementById("btnValiderMot")
+console.log(validationButton)
+
+let proposedWord = document.querySelector(".zoneProposition")
+console.log(proposedWord)
+
+let scoreDisplay = document.querySelector(".zoneScore span")
+console.log(scoreDisplay) 
+
+let btnChoice = document.querySelectorAll(".optionSource input")
+console.log(btnChoice)
+
+let nouveauTitre = document.createElement("h2")
+let headerTag = document.querySelector("header")
+console.log(headerTag)
+headerTag.appendChild(nouveauTitre)
+nouveauTitre.textContent = "... vraiment plus vite !"
